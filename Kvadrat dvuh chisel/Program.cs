c76e984ea75336f00e6596fd6a59be29d0f296e6
Roman <79613377185R@gmail.com>
2022-09-17 00:26:17 +0300

    Kvadrat Chisla

--- Kvadrat dvuh chisel/Program.cs
+++ Kvadrat dvuh chisel/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿Console.WriteLine("Vvedite Chislo");
+int Usernumber = Convert.ToInt32(Console.ReadLine());
+int result = (Usernumber * Usernumber);
+Console.WriteLine($"Kvadrat chisla {Usernumber} raven {result}");
