79b93dec5a32b4fbc7ec73783450dc35a8aaccf1
Roman <79613377185R@gmail.com>
2022-09-17 01:41:01 +0300

    Pervoe Kvadrat net

---
+++ Kvadrat li Pervoe/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+Console.WriteLine("VVedite Pervoe Chislo");
+int NumX  = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("VVedite Vtoroe Chislo");
+int NumY = Convert.ToInt32(Console.ReadLine());
+int Kvadrat = (NumY * NumY);
+if (Kvadrat == NumX)
+{
+    Console.WriteLine($"Chislo {NumX} Kvadrat Chisla {NumY}");
+}
+else
+{
+    Console.WriteLine($"Chislo {NumX} NE Kvadrat Chisla {NumY}");
+}
