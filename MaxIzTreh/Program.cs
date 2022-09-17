7b8057dd868121a8f9d041f91655c9c8ea4652bd
Roman <79613377185R@gmail.com>
2022-09-17 17:10:29 +0300

    Max iz treh

--- MaxIzTreh/Program.cs
+++ MaxIzTreh/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+Console.WriteLine("Vvedite Chislo");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Vvedite Vtoroe Chislo");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Vvedite Tretye Chislo");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (max < num2) max = num2;
+else if (max < 3 ) max = num3;
+Console.WriteLine($"Max {max}");
