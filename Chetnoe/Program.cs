f1c0179a9eac2cffdbba3f7cbe9ef9b36db97979
Roman <79613377185R@gmail.com>
2022-09-17 17:37:30 +0300

    Chet ne chet

---
+++ Chetnoe/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка)
+Console.WriteLine("Vvedite Celoe Chislo");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num % 2;
+if ( result > 0 ) Console.WriteLine("NET");
+else Console.WriteLine("DA");
