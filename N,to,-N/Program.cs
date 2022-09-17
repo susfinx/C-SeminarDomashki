246af983283bda92d1e5174d4ad908cdb8fd910c
Roman <79613377185R@gmail.com>
2022-09-17 15:15:45 +0300

    N to -N

---
+++ N,to,-N/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+Console.WriteLine("VVedite Chislo");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = -num;
+while ( count <= num )
+{
+    Console.Write($"{count}, ");
+    count++;
+}
