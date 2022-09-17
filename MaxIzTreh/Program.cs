193e73215d164d5b6a5e3fec61ff74a2b7104983
susfinx <113091653+susfinx@users.noreply.github.com>
2022-09-18 01:06:57 +0300

    popravil

--- MaxIzTreh/Program.cs
+++ MaxIzTreh/Program.cs
@@ -7,5 +7,5 @@ Console.WriteLine("Vvedite Tretye Chislo");
 int num3 = Convert.ToInt32(Console.ReadLine());
 int max = num1;
 if (max < num2) max = num2;
-else if (max < 3 ) max = num3;
+else if (max < num3 ) max = num3;
 Console.WriteLine($"Max {max}");
