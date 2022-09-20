c826adb5bb38a5239da589e29b91d76fb98055ca
Roman <79613377185R@gmail.com>
2022-09-20 16:54:14 +0300

    delet srednee

--- Sluchaynoe trehznachnoe/Program.cs
+++ Sluchaynoe trehznachnoe/Program.cs
@@ -1,2 +1,18 @@
 ﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
 
+int getRandomNumberFromRange(int minBorder, int maxBorder)
+{
+    int result = new int();
+    result = new Random().Next(minBorder, maxBorder +1);
+    return result;
+}
+int randomnumber = getRandomNumberFromRange(100, 999);
+int firstnumber = randomnumber/100;
+int lastnumber = randomnumber%10;
+int firstPlusLast = firstnumber*10+lastnumber;
+
+
+
+
+Console.WriteLine($"Sluchaynoe Znachenie {randomnumber}");
+Console.WriteLine (firstPlusLast);
