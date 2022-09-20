5bca65be2d9af942dcc91e838f473528452b3c93
Roman <79613377185R@gmail.com>
2022-09-20 15:22:02 +0300

    sluchainoe chislo i max digit

--- rondom digit max/Program.cs
+++ rondom digit max/Program.cs
@@ -1,5 +1,30 @@
 ﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99]
 // и показывает наибольшую цифру числа. 78 -> 8 12-> 2 85 -> 8
 
-int Rondom Next(10, 99);
-Console.WriteLine()
+int getRandomNumberFromRange(int minBorder, int maxBorder)
+{
+    int result = new int();
+    result = new Random().Next(minBorder, maxBorder +1);
+    return result;
+}
+int getMaxDigitFromNumber (int number)
+{   
+    int maxDigit = 0;
+    while (number>0)
+    {
+        int currentDigit = number %10;
+        if(maxDigit < currentDigit)
+        {
+            maxDigit = currentDigit;
+        }
+        number = number / 10;
+    }
+    return maxDigit;
+}
+int randomNumber = getRandomNumberFromRange(109,5999);
+Console.WriteLine($"Sluchaynoe Znachenie {randomNumber}");
+int maxDigit = getMaxDigitFromNumber(randomNumber);
+Console.WriteLine($"Dlya chisla {randomNumber} bolshaya cifra {maxDigit}");
+
+
+
