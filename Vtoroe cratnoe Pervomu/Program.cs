1f57e4b78df600f608298631d9e110a6e8878859
Roman <79613377185R@gmail.com>
2022-09-20 20:45:05 +0300

    vtoroe kratnoe pervomu

---
+++ Vtoroe cratnoe Pervomu/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая будет принимать на вход два числа
+// и выводить, является ли второе число кратным первому.
+Console.WriteLine("VVedite Pervoe Chislo ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("VVedite Vtoroe Chislo ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int result = num1 % num2;
+if (result==0) Console.WriteLine("Da");
+else  Console.WriteLine("Net");
