476fe741e8bfa0223e7103d2e3ef643ed35d0ef5
Roman <79613377185R@gmail.com>
2022-09-20 21:39:35 +0300

    Kratno li 7 i 23

---
+++ cratno7i23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+Console.WriteLine("Vvedite chislo!");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+string CratnoLichislo (int Number)
+{
+    string Kratno = "";
+    if ( Number % 7 == 0 && Number % 23 ==0 )
+    {
+        Kratno = "DA";
+    } 
+    else Kratno = "Net";
+    return Kratno;
+}
+string result = CratnoLichislo (Number);
+Console.WriteLine(result);
