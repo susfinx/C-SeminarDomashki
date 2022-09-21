3e7b827f9ace307994f86621f91c95fe1956e343
Roman <79613377185R@gmail.com>
2022-09-21 19:03:08 +0300

    Tretya,tretyey net

---
+++ Tretya Zadanogo Chisla/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Vvedite TREHznachnoe cislo");
+int Chislo = Convert.ToInt32(Console.ReadLine());
+if (Chislo < 100 || Chislo > 999) Console.WriteLine("Tretyey Cifry Net ili chislo Ne treh znacnoe");
+else Console.WriteLine(Chislo % 10);
+
