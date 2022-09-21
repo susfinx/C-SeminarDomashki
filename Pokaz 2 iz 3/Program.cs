bef2844ca3be6951f636093d3379688d0442cc8b
Roman <79613377185R@gmail.com>
2022-09-21 18:38:04 +0300

    Na vyhode Vtoraya

---
+++ Pokaz 2 iz 3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Number = new Random().Next(100, 999+1);
+
+int Pokaz2Chisla ( int Number)
+{   
+    int Pokaz = Number / 10 % 10;
+    return Pokaz;
+}
+Console.WriteLine(Number);
+int vtoraya = Pokaz2Chisla (Number);
+Console.WriteLine(vtoraya);
