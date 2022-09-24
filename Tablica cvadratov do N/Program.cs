198ad5f2431b8012aca1f3db6b6226f7bc89289a
Roman <79613377185R@gmail.com>
2022-09-24 21:21:13 +0300

    Tablica Kvadratov do N

---
+++ Tablica cvadratov do N/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов 
+//чисел от 1 до N. 5 -> 1, 4, 9, 16, 25. 2 -> 1,4
+
+
+Console.WriteLine("VVedite Chislo");
+int Number = Convert.ToInt32(Console.ReadLine());
+if ( Number < 0 )
+{
+    Console.WriteLine("Vvedite celoe, Polojitelnoe chislo");
+}
+int Count = 0;
+int ChislaDoN = 1;
+
+while ( ChislaDoN < Number )
+{
+        
+    int qvadraty = Convert.ToInt32(Math.Pow(ChislaDoN, 2));
+
+
+    Console.Write($"{qvadraty}, ");
+    ChislaDoN++;
+    Count++;
+             
+}
