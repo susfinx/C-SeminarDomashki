6931d435f4a7ac1868a11dd3994e6766f78f73a1
Roman <79613377185R@gmail.com>
2022-09-24 17:09:07 +0300

    Rastoyanie mezhdu tochek 2 D

---
+++ 2 d Rastoyanie tochek /Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 21: Напишите программу, которая принимает на вход координаты 
+//двух точек и находит расстояние между ними в 2D пространстве. A (3,6); B (2,1) -> 5,09 A (7,-5); B (1,-1) -> 7,21
+
+double FormulaDlinny(int firstTochkaX, int firstTochkaY, int secondTochkaX, int secondTochkaY)
+{
+    double result= Math.Sqrt(Math.Pow(firstTochkaX-secondTochkaX, 2) + Math.Pow(firstTochkaY-secondTochkaY, 2));
+    return result;
+} 
+
+
+Console.WriteLine("vvedite X coordinat Pervoy tochki");
+int firstTochkaX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("vvedite Y coordinat Pervoy tochki");
+int firstTochkaY = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("vvedite X coordinat vtoroy tochki");
+int secondTochkaX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("vvedite Y coordinat vtoroy tochki");
+int secondTochkaY = Convert.ToInt32(Console.ReadLine());
+double Dlinnamejdutochkami = FormulaDlinny(firstTochkaX, firstTochkaY, secondTochkaX, secondTochkaY);
+Console.WriteLine(Dlinnamejdutochkami);
