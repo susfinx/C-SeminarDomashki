7ebcb0fd6bf48d25bd02ffee7ba6848140ed1309
Roman <79613377185R@gmail.com>
2022-09-24 13:43:52 +0300

    X_Y Chetvert

--- X_Y_ne=0/Program.cs
+++ X_Y_ne=0/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
+// в которой находится эта точка.
+
+int getquartercoordinate (int usercoordinatX, int usercoordinatY)
+
+{
+    int result = 0;
+    if (usercoordinatX > 0 && usercoordinatY > 0)
+    {
+        result = 1;
+    }
+    else if (usercoordinatX< 0 && usercoordinatY > 0)
+    {
+        result = 2;
+    }
+    else if (usercoordinatX< 0 && usercoordinatY < 0)
+    {
+        result = 3;
+    }
+        else if (usercoordinatX > 0 && usercoordinatY < 0)
+    {
+        result = 4;
+    }
+
+    return result;
+}
+
+int getProverkaNaDuraka (string userinformation)
+{
+    int result = 0;
+    while (result == 0)
+    {
+        Console.Write(userinformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0" )
+        {
+            Console.WriteLine ($"VVedite celoe chislo, Vy Vveli {userLine} Eto ne celoe chislo");
+        }
+
+    } return result;
+}
+int usercoordinatX = 0;
+int usercoordinatY = 0;
+
+Console.WriteLine("VVedite Coordinaty");
+usercoordinatX = getProverkaNaDuraka ("X: ");
+usercoordinatY = getProverkaNaDuraka ("Y: ");
+
+int quarter = getquartercoordinate (usercoordinatX, usercoordinatY);
+
+Console.WriteLine($"Tochka s koordinatami {usercoordinatX} : {usercoordinatY} Nahoditsya v {quarter} ploskosti");
