d5cd552675c3edb4a19e7fcd1001ab4cc05b1e62
Roman <79613377185R@gmail.com>
2022-09-25 19:03:40 +0300

    Zadacha s Izmeneniem teksta cherez for

---
+++ Lekciya/Program.cs
@@ -0,0 +1,21 @@
+﻿
+//string Text = (— Я думаю, — сказал князь, улыбаясь, — что, ежели бы вас послали вместо нашего милого Винценгероде, вы бы взяли приступом согласие прусского короля.Вы так красноречивы. Вы дадите мне чаю?;
+string text = "Я думаю, — сказал князь, улыбаясь, — что, ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля.Вы так красноречивы. Вы дадите мне чаю?";
+              
+string Replase (string text, char oldValue, char newValue)
+{
+    
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result= result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replase (text, ' ', '|');
+Console.WriteLine(newText);
