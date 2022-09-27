3f696e2971ebeced2cfbcdab48cf04eae4302416
Roman <79613377185R@gmail.com>
2022-09-28 00:09:09 +0300

    zadacha 24 lelciya summa ot odnogo do N

---
+++ A summa chisel ot 1/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+
+int getSumofRanch (int startPoint, int endPoint)
+{
+    int result = 0;
+    for (int i = startPoint; i<= endPoint; i++)
+    {
+        result +=i;
+    }
+
+
+    return result;
+}
+
+Console.WriteLine("Vvedite Chislo");
+int usernumber = Convert.ToInt32(Console.ReadLine());
+int sumOfRange = getSumofRanch (1, usernumber);
+Console.WriteLine($"Summa Chisel ot 1 do {usernumber} = {sumOfRange}");
+
+
