6d41605e480e10dfee8e53e5cbe0310b825b414f
Roman <79613377185R@gmail.com>
2022-09-28 23:27:31 +0300

    Zadacha 29

---
+++ Zadacha 29  DZ/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Напишите программу, в которой пользователь задает длину массива, элементы которого задаются в диапазоне [1,99] и выводит на экран .
+
+5 -> [1, 2, 5, 7, 19]
+
+3 -> [6, 1, 33] */
+
+void printeArray(int[]array)
+{
+    Console.Write("[");
+    for (int i =0;i<array.Length;i++) 
+    {
+        Console.Write(array[i]);
+        if (i <array.Length -1);
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+
+
+
+Console.WriteLine("Zadayte dlinnu massiva!");
+int DlnnaMassiva = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [DlnnaMassiva];
+for (int i =0;i<array.Length;i++)
+{
+    array[i]= new Random().Next(1,100);
+}
+
+printeArray (array);
