5282f6d524a5cf055544a3197d3aa60902c662e2
Roman <79613377185R@gmail.com>
2022-10-03 21:18:30 +0300

    Zadacha 36 DZ

---
+++ Zadacha 36 DZ/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+Console.WriteLine("Hello, World!"); */
+
+int[] getRandomArray (int length, int startPoint, int endPoint)
+{
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i]= new Random().Next(startPoint, endPoint +1);
+    }
+    return resultArray;
+}
+
+void printArray (int[] incomingArray)
+{
+    Console.Write ("[");
+    for (int i = 0; i<incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i<incomingArray.Length -1)
+        {
+            Console.Write(",");
+        }
+    }Console.WriteLine("]");
+    
+} 
+
+
+int getsumma (int [] PoluchiliMassiv)
+{
+    int SummaNechet =0;
+    for ( int i = 0; i< PoluchiliMassiv.Length; i=i+2)
+    {
+        SummaNechet= SummaNechet+ PoluchiliMassiv[i];
+    }
+    return SummaNechet;
+}   
+
+int [] PoluchiliMassiv = getRandomArray (10, 0,100);
+printArray (PoluchiliMassiv);
+int SummaChiselNechetnyhPoz = getsumma (PoluchiliMassiv);
+Console.WriteLine($"Summa nechetnyh Poziciy Massiva ravna {SummaChiselNechetnyhPoz}");
