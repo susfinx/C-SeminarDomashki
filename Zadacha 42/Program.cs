7bc0be1764f03ffd5749cf4f3b618823baedcaeb
Roman <79613377185R@gmail.com>
2022-10-05 19:01:50 +0300

    Convert iz desetichnogo koda v dvoichnyi.

---
+++ Zadacha 42/Program.cs
@@ -0,0 +1,51 @@
+﻿/* Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10
+*/
+int getUserNumber (string userInformation)    // Poluchenie chisla ot Usera
+{
+    int result;
+    Console.WriteLine ("VVedite Chislo");
+    while (!int.TryParse(Console.ReadLine(),out result)|| result <=0)
+    {
+        Console.WriteLine ("Oshibka vvoda ");
+    }
+    return result;
+} 
+
+int getLengthArray (int number)
+{
+    int count=0;
+    while (number!=0)
+    {
+        number /= 2;
+        count++;
+    }
+    return count;
+}
+
+bool [] getBoolArray (int number)           // Convertaciya v dvoichnyi cod
+{
+    int length = getLengthArray (number);
+    bool[]boolArray = new bool [length];
+    for (int index =0; index <length; index++)
+    {
+        boolArray [length-1-index]=number%2!=0;
+        number/=2;
+    }
+    return boolArray;
+}
+
+void printboolarray (bool[] array)
+{
+    for (int i=0;i<array.Length; i++)
+    {
+        Console.Write (array[i]?"1":"0");
+    }
+    Console.WriteLine();
+}
+
+int number = getUserNumber ("Vvedite Chislo");
+bool [] boolArray = getBoolArray (number);
+printboolarray (boolArray);
