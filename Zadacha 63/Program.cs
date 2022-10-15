69724d1a9fd3a646dc32723ecef773bc771ad45b
Roman <79613377185R@gmail.com>
2022-10-15 23:59:28 +0300

    Zadacha63

---
+++ Zadacha 63/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные 
+числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6
+*/
+
+Console.WriteLine ("VVedite Chislo");
+int userNumber= Convert.ToInt32(Console.ReadLine());
+printRange(userNumber);
+
+void printRange (int N)
+{
+    if ( N == 0)
+    {
+        return;
+    }
+    printRange(N-1);
+    Console.Write($"{N}, ");
+}
