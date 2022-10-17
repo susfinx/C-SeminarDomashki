b428db412f623ae71656e7483d1a56727e685b1a
Roman <79613377185R@gmail.com>
2022-10-17 13:14:47 +0300

    Zadacha 67

---
+++ Zadacha 67/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 
+Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+Console.WriteLine ("VVedite Chislo");
+int userNumber= Convert.ToInt32(Console.ReadLine());
+int sum=0;
+int sumdig = GetSummdigit (userNumber);
+Console.WriteLine(sumdig);
+int GetSummdigit (int userNumber)
+{   
+    if(userNumber==0)
+    {
+        return sum;
+    }
+    sum+=userNumber%10;
+    GetSummdigit (userNumber/10);
+    return sum;
+}
